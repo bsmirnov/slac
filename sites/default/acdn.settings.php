146376212b748c2e311c9fde0e95e51cad5045b2
Jimmy Pham <jpham@slac.stanford.edu>
2022-08-22 18:49:10 -0700

    update acdn.settings.php per Ken from ticket 699085

--- sites/default/acdn.settings.php
+++ sites/default/acdn.settings.php
@@ -10,7 +10,7 @@
  * Headers that will be accessed for domain masking.
  */
 $varyHeaders = [
-  'orig-host',
+  'x-host',
   'x-masked-path',
 ];
 
@@ -28,15 +28,19 @@ $varyString = implode(', ', $varyHeaders);
 header("Vary: ${varyString}", FALSE);
 
 if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
-  if (isset($_SERVER['HTTP_ORIG_HOST'])) {
-    $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_ORIG_HOST'];
+  // HTTP_ORIG_HOST is still present but is deprecated in favor of HTTP_X_HOST
+  if (isset($_SERVER['HTTP_X_HOST'])) {
+    $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_HOST'];
+
   }
 
   // Now also handle subpath stuff.
   if (isset($_SERVER['HTTP_X_MASKED_PATH'])) {
     $base_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['HTTP_X_MASKED_PATH'];
     $base_path = $_SERVER['HTTP_X_MASKED_PATH'];
-    $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_ORIG_HOST'] . $_SERVER['HTTP_X_MASKED_PATH'];
-    $cookie_domain = $_SERVER['HTTP_ORIG_HOST'];
+
+    # set cookie path to the value of HTTP_X_MASKED_PATH to avoid collision if parent name uses the same cookie name
+    $cookieParams = session_get_cookie_params(); 
+    session_set_cookie_params($cookieParams['lifetime'], $_SERVER['HTTP_X_MASK_PAST'], $cookieParams['domain'], $cookieParams['secure'], $cookieParams['httponly'], $cookieParams['samesite']);
   }
 }
