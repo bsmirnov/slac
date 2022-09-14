0ad2492227bb2db42936e455e46afb005b74a9a7
Rachel Whitton <rachel@pantheon.io>
2022-09-14 11:50:40 -0500

    Fixup acdn saml config

--- private/simplesamlphp/config/acdn_config.php
+++ private/simplesamlphp/config/acdn_config.php
@@ -5,6 +5,6 @@
  * AGCDN domain masking settings.
  */
 
-if (isset($_SERVER['HTTP_ORIG_HOST']) && isset($_SERVER['HTTP_X_MASKED_PATH'])) {
-  $config['baseurlpath'] = 'https://' . $_SERVER['HTTP_ORIG_HOST'] . $port . $_SERVER['HTTP_X_MASKED_PATH'] . '/simplesaml/';
+if (isset($_SERVER['HTTP_X_HOST']) && isset($_SERVER['HTTP_X_MASKED_PATH'])) {
+  $config['baseurlpath'] = 'https://' . $_SERVER['HTTP_X_HOST']  . $_SERVER['HTTP_X_MASKED_PATH'] . '/simplesaml/';
 }
