699b6c95f1ecf08bfe7540579e22c72461022569
Rachel Whitton <rachel@pantheon.io>
2022-09-19 18:42:46 -0500

    Fix typo

--- private/simplesamlphp/config/acdn_config.php
+++ private/simplesamlphp/config/acdn_config.php
@@ -7,5 +7,5 @@
 
  if (isset($_SERVER['HTTP_X_HOST']) && isset($_SERVER['HTTP_X_MASKED_PATH'])) {
    $config['baseurlpath'] = 'https://' . $_SERVER['HTTP_X_HOST']  .':443'. $_SERVER['HTTP_X_MASKED_PATH'] . '/simplesaml/';
-   $config['session.cookie.secure'] =  $_SERVER['HTTP_X_MASKED_PATH'],
+   $config['session.cookie.secure'] =  $_SERVER['HTTP_X_MASKED_PATH'];
  }
